ca52707349b71e10839fdf1da20e0bd2698343c2
Daniel Dworakowski <dworakowskidaniel@gmail.com>
2018-03-11 23:23:06 -0400

    added more of the paper

--- ComplementaryFilter.py
+++ ComplementaryFilter.py
@@ -10,7 +10,15 @@ class ComplementaryFilter(object):
 
     def __init__(self, initialReading):
         # 
-        # Initial guess for the orientation will be a direct calculation.
+        # Filter parameters.
+        self.alpha = 0.01
+        self.beta = 0.01
+        self.gyroBias = np.zeros((3,1))
+        self.prev_imumsg = ImuMsg()
+        self.g = 9.81
+        # 
+        # Initial guess for the orientation will be a direct calculation
+        # as described in section 5.4.
         acc_norm = initialReading.acc / np.norm(initialReading.acc)
         a_x, a_y, a_z = acc_norm
         # 
@@ -48,6 +56,7 @@ class ComplementaryFilter(object):
         self.q_body =  q_acc * q_mag
         self.q_t_ = self.q_body
 
+
     def predict(self, gyro, dt):
         # 
         # Eq 40.
@@ -61,6 +70,36 @@ class ComplementaryFilter(object):
         # Eq 42. 
         return q_t_ + np.dot(omega, q_t_) * dt
 
+    def _interpolate(self, q2, gain):
+        qI = np.quaternion(1, 0, 0, 0)
+        # 
+        # Eq 50.
+        qbar = None
+        if q2[0] > 0.9:
+            qbar = (1 - gain) * qI + gain * q2
+        else:
+            # 
+            # Eq 48.
+            omega = np.acos(q2[0])
+            qbar = np.sin((1 - gain) * omega) / np.sin(omega) * qI + np.sin(gain * omega) / np.sin(omega) * q2
+        # 
+        # Eq 51.
+        qHat = qbar / np.norm(qbar)
+        return qHat
+
+    def adaptiveGain(self, acc):
+        # 
+        # Eq 60.
+        e_m  = np.abs(np.norm(acc) - self.g) / self.g
+        # 
+        # Fig 5. 
+        if e_m < 0.1:
+            return 1
+        elif e_m > 0.2:
+            return 0
+        else:
+            return -10 * e_m + 2
+
     def correct(self, imumsg, q_pred):
         # 
         # Eq 44.
@@ -69,20 +108,65 @@ class ComplementaryFilter(object):
         g_pred = np.dot(quaternion.as_rotation_matrix(quaternion.inverse()), acc)
         # 
         # Eq 47.
-        g_x, g_y, g_z
+        g_x, g_y, g_z = g_pred
         dq_0 = np.sqrt((g_z + 1) / 2)
         dq_1 = - g_y / (np.sqrt(2 * (g_z + 1)))
         dq_2 = g_x / (np.sqrt(2 * (g_z + 1)))
         dq_3 = 0
         dq = np.quaternion(dq_0, dq_1, dq_2, dq_3)
+        # 
+        # Correct dq.
+        alpha = self.adaptiveGain(imumsg.acc)
+        dqHat = self._interpolate(dq, alpha)
+        # 
+        # Update q_pred.
+        q_pred_update = q_pred * dqHat
+        # 
+        # Rotate the magnetice field vector.
+        # Eq 54.
+        l = np.dot(quaternion.as_rotation_matrix(q_pred_update).T, imumsg.mag)
+        l_x, l_y, l_z = l
+        # 
+        # Calculate the delta quaternion Eq 58.
+        gamma = l_x ** 2 + l_y ** 2
+        dq_0 = np.sqrt(gamma + l_x * np.sqrt(gamma)) / np.sqrt(2 * gamma)
+        dq_1 = 0
+        dq_2 = 0
+        dq_3 = l_y / np.sqrt(2 * (gamma + l_x * np.sqrt(gamma)))
+        dq = np.quaternion(dq_0, dq_1, dq_2, dq_3)
+        dqHat = self._interpolate(dq, self.beta)
+        # 
+        # Eq 59.
+        q_pred_update = q_pred_update * dqHat
+        # 
+        # Normalize.
+        self.q_body = q_pred_update / np.norm(q_pred_update)
 
+    def _estimateBias(self, imumsg):
+        # 
+        # Estimate the bias here.
+        #######
+        #######
+        #######
+        # 
+        # Return the message with the bias removed.
+        unbiased = imumsg
+        unbiased.gyro -= self.gyroBias
+        return imumsg
 
     def observation(self, imumsg, dt):
         # 
         # Update the last measurement. 
         self.q_t_ = self.q_body
         # 
+        # Bias estimation.
+        filtered = self._estimateBias(imumsg)
+        # 
         # Prediction. 
-        q_pred = self.predict(imumsg.gyro, dt)
+        q_pred = self.predict(filtered.gyro, dt)
         # 
         # Correction. 
+        self.q_body = self.correct(filtered, q_pred)
+        # 
+        # Store message as previous.
+        self.prev_imumsg = imumsg
